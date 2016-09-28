acfc49ac3eb0d86b01df6afbbb0aa8c71c2c4a3f
Matthew Khouzam <matthew.khouzam@ericsson.com>
2016-09-28 13:55:51 -0400

    timing.ui: add Export to TSV to tables and statistics
    This allows exporting to tsv files. This will incur code
    duplication due to the similarities between Tree and Table.
    
    Change-Id: I506d0e7e50bd5ce8ecbd44675a0c404ab2dda431
    Signed-off-by: Matthew Khouzam <matthew.khouzam@ericsson.com>

--- analysis/org.eclipse.tracecompass.analysis.timing.ui/src/org/eclipse/tracecompass/analysis/timing/ui/views/segmentstore/statistics/AbstractSegmentStoreStatisticsView.java
+++ analysis/org.eclipse.tracecompass.analysis.timing.ui/src/org/eclipse/tracecompass/analysis/timing/ui/views/segmentstore/statistics/AbstractSegmentStoreStatisticsView.java
@@ -11,12 +11,23 @@
  *******************************************************************************/
 package org.eclipse.tracecompass.analysis.timing.ui.views.segmentstore.statistics;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.jface.action.Action;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Tree;
+import org.eclipse.swt.widgets.TreeItem;
 import org.eclipse.tracecompass.common.core.NonNullUtils;
 import org.eclipse.tracecompass.tmf.core.trace.ITmfTrace;
 import org.eclipse.tracecompass.tmf.core.trace.TmfTraceManager;
 import org.eclipse.tracecompass.tmf.ui.views.TmfView;
+
+import com.google.common.base.Joiner;
 
 /**
  * Abstract view to to be extended to display segment store statistics.
@@ -26,6 +37,54 @@ import org.eclipse.tracecompass.tmf.ui.views.TmfView;
  */
 public abstract class AbstractSegmentStoreStatisticsView extends TmfView {
 
+    private final Action fExportAction = new Action() {
+        private final String[] fExtensions = { "*.tsv", "*.*" };//$NON-NLS-1$//$NON-NLS-2$
+
+        @Override
+        public void run() {
+            AbstractSegmentStoreStatisticsViewer segmentStoreViewer = fStatsViewer;
+            if (segmentStoreViewer == null) {
+                return;
+            }
+            FileDialog fd = new FileDialog(getViewSite().getShell());
+            fd.setFilterExtensions(fExtensions);
+            String fileName = fd.open();
+            try (PrintWriter pw = new PrintWriter(fileName)) {
+                Tree tree = segmentStoreViewer.getTreeViewer().getTree();
+                int size = tree.getItemCount();
+                List<String> columns = new ArrayList<>();
+                for (int i = 0; i < tree.getColumnCount(); i++) {
+                    columns.add(tree.getColumn(i).getText());
+                }
+                pw.println(Joiner.on('\t').join(columns));
+                for (int i = 0; i < size; i++) {
+                    TreeItem item = tree.getItem(i);
+                    printItem(pw, columns, item);
+                }
+            } catch (FileNotFoundException e) {
+
+            }
+        }
+
+        private void printItem(PrintWriter pw, List<String> columns, @Nullable TreeItem item) {
+            if (item == null) {
+                return;
+            }
+            List<String> data = new ArrayList<>();
+            for (int col = 0; col < columns.size(); col++) {
+                data.add(item.getText(col));
+            }
+            pw.println(Joiner.on('\t').join(data));
+            for( TreeItem child : item.getItems()){
+                    printItem(pw, columns, child);
+            }
+        }
+
+        @Override
+        public String getText() {
+            return "Export to TSV";
+        }
+    };
     @Nullable private AbstractSegmentStoreStatisticsViewer fStatsViewer = null;
 
     /**
@@ -44,6 +103,7 @@ public abstract class AbstractSegmentStoreStatisticsView extends TmfView {
             statsViewer.loadTrace(trace);
         }
         fStatsViewer = statsViewer;
+        getViewSite().getActionBars().getMenuManager().add(fExportAction);
     }
 
     @Override
--- analysis/org.eclipse.tracecompass.analysis.timing.ui/src/org/eclipse/tracecompass/analysis/timing/ui/views/segmentstore/table/AbstractSegmentStoreTableView.java
+++ analysis/org.eclipse.tracecompass.analysis.timing.ui/src/org/eclipse/tracecompass/analysis/timing/ui/views/segmentstore/table/AbstractSegmentStoreTableView.java
@@ -13,12 +13,23 @@
 
 package org.eclipse.tracecompass.analysis.timing.ui.views.segmentstore.table;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.jdt.annotation.Nullable;
+import org.eclipse.jface.action.Action;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.SashForm;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableItem;
 import org.eclipse.tracecompass.tmf.ui.views.TmfView;
+
+import com.google.common.base.Joiner;
 
 /**
  * View for displaying a segment store analysis in a table.
@@ -27,6 +38,44 @@ import org.eclipse.tracecompass.tmf.ui.views.TmfView;
  * @since 2.0
  */
 public abstract class AbstractSegmentStoreTableView extends TmfView {
+
+    private final Action fExportAction = new Action() {
+        private final String[] fExtensions = { "*.tsv", "*.*" };//$NON-NLS-1$//$NON-NLS-2$
+
+        @Override
+        public void run() {
+            AbstractSegmentStoreTableViewer segmentStoreViewer = getSegmentStoreViewer();
+            if (segmentStoreViewer == null)
+                return;
+            FileDialog fd = new FileDialog(getViewSite().getShell());
+            fd.setFilterExtensions(fExtensions);
+            String fileName = fd.open();
+            try (PrintWriter pw = new PrintWriter(fileName)) {
+                Table table = segmentStoreViewer.getTableViewer().getTable();
+                int size = table.getItemCount();
+                List<String> columns = new ArrayList<>();
+                for (int i = 0; i < table.getColumnCount(); i++) {
+                    columns.add(table.getColumn(i).getText());
+                }
+                pw.println(Joiner.on('\t').join(columns));
+                for (int i = 0; i < size; i++) {
+                    TableItem item = table.getItem(i);
+                    List<String> data = new ArrayList<>();
+                    for (int col = 0; col < columns.size(); col++) {
+                        data.add(item.getText(col));
+                    }
+                    pw.println(Joiner.on('\t').join(data));
+                }
+            } catch (FileNotFoundException e) {
+
+            }
+        }
+
+        @Override
+        public String getText() {
+            return "Export to TSV";
+        }
+    };
 
     // ------------------------------------------------------------------------
     // Attributes
@@ -54,6 +103,7 @@ public abstract class AbstractSegmentStoreTableView extends TmfView {
         SashForm sf = new SashForm(parent, SWT.NONE);
         TableViewer tableViewer = new TableViewer(sf, SWT.FULL_SELECTION | SWT.VIRTUAL);
         fSegmentStoreViewer = createSegmentStoreViewer(tableViewer);
+        getViewSite().getActionBars().getMenuManager().add(fExportAction);
         setInitialData();
     }
 
